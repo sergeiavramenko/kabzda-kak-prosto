43badf91af00dd0afecbb0532cb725511cd3d715
Sergei <sergei.avramenko199@yandex.ru>
2023-01-31 21:07:19 +0300

    import

---
+++ kabzda/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div>
+<header />
+<technologies />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ kabzda/src/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div> <a href="#">Home</a>
+        <a href="#">News</a>
+        <a href="#">Massages</a>
+        </div>
+
+    );
+}
+export default Header;
